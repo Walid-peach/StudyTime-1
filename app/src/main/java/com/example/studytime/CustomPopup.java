19fc0c356b7f282da34e1a00fbe717493743ac7a
Walid-peach <77234901+Walid-peach@users.noreply.github.com>
2021-05-30 14:27:24 +0000

    Pop Up

---
+++ app/src/main/java/com/example/studytime/CustomPopup.java
@@ -0,0 +1,12 @@
+package com.example.studytime;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class CustomPopup extends Dialog {
+    public CustomPopup(@NonNull Context context) {
+        super(context);
+    }
+}
