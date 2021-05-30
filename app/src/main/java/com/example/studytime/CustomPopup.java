fe050adee4c19581a72ddfc988a60be2b21596f2
Walid-peach <77234901+Walid-peach@users.noreply.github.com>
2021-05-30 15:16:00 +0000

    pop up

--- app/src/main/java/com/example/studytime/CustomPopup.java
+++ app/src/main/java/com/example/studytime/CustomPopup.java
@@ -1,12 +1,26 @@
 package com.example.studytime;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 public class CustomPopup extends Dialog {
-    public CustomPopup(@NonNull Context context) {
-        super(context);
+
+    private ImageView image;
+    private TextView title;
+    private Button place1, place2, place3, place4, place5, place6, place7, place8;
+
+
+    public CustomPopup(Activity activity){
+        super (activity, R.style.Theme_AppCompat_DayNight_Dialog);
+        setContentView(R.layout.my_popup);
+
+
     }
+
 }
